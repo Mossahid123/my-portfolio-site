aa77f821e94bd01c828ebfdf42a624c0f127b3aa
Mossahid123 <mossahidhossain309@gmail.comm>
2022-08-31 11:52:25 +0600

    home page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
 import Navbar from "./Pages/Shared/Navber/Navbar";
 
 function App() {
   return (
     <>
      <Navbar />
+     <Routes>
+     <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+     </Routes>
     </>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Particle from '../../../Components/Particles/Particles';
+
+const Home = () => {
+    return (
+        <div>
+            <Particle />
+        </div>
+    );
+};
+
+export default Home;
